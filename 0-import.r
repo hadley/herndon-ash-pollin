3b8a495a7bc6049d5d961647cf4c99af07ceb923
hadley <h.wickham@gmail.com>
2013-05-08 08:09:18 -0500

    Rewrite processing code. And save in csv, not opaque binary format

--- 0-import.r
+++ 0-import.r
@@ -4,44 +4,44 @@ csvs <- dir("RR-country-csv", full.name = TRUE)
 all <- ldply(csvs, read.csv, stringsAsFactors = FALSE)
 
 count(all, "Country")
-
 write.csv(all, "RR-basic.csv", row.names = FALSE)
 
-RR <- within(RR, debtgdp <- ifelse(Country=="Australia",ifelse(Year<=1948,100*Debt/GDP1,100*Debt/GDP2),debtgdp))
-RR <- within(RR, debtgdp <- ifelse(Country=="Austria",ifelse(Year<=1979,100*Debt1/GDP1,100*Debt2/GDP2),debtgdp))
-RR <- within(RR, debtgdp <- ifelse(Country=="Belgium",ifelse(Year<=1979,100*Debt/GDP1,100*Debt/GDP2),debtgdp))
-RR <- within(RR, debtgdp <- ifelse(Country=="Canada",ifelse(Year<=1948,100*Debt/GDP1,100*Debt/GDP2),debtgdp))
-RR <- within(RR, debtgdp <- ifelse(Country=="Denmark",ifelse(Year<=1949,100*Debt1/GDP1,100*Debt1/GDP2),debtgdp))
-RR <- within(RR, debtgdp <- ifelse(Country=="Finland",ifelse(Year<=1977,100*Debt1/GDP1,100*Debt2/GDP2),debtgdp))
-RR <- within(RR, debtgdp <- ifelse(Country=="France",ifelse(Year<=1948, 100*Debt1 / GDP1, ifelse(Year<=1978, 100*Debt1 / GDP2,100*Debt2/GDP2)),debtgdp))
-RR <- within(RR, debtgdp <- ifelse(Country=="Germany",ifelse(Year<=1950,100*Debt1/GDP1,100*Debt2/GDP2),debtgdp))
-RR <- within(RR, debtgdp <- ifelse(Country=="Greece",ifelse((Year>=1884 & Year<=1913) | (Year>=1919 & Year<=1939) | (Year>=1970 & Year<=1992),100*Debt/GDP1, ifelse(Year==2009,100,debtgdp)),debtgdp))
-RR <- within(RR, debtgdp <- ifelse(Country=="Ireland",ifelse(Year<=2010,100*Debt/GDP2,debtgdp),debtgdp))
-RR <- within(RR, debtgdp <- ifelse(Country=="Italy",ifelse(Year<=1913,100*Debt/GDP1,ifelse(Year<=1946,100*Debt/GNI,ifelse(Year<=1998,100*Debt/GDP1,100*Debt/GDP2))),debtgdp))
-RR <- within(RR, debtgdp <- ifelse(Country=="Japan",ifelse(Year<=1953,100*Debt/GDP1,100*Debt/GDP2),debtgdp))
-RR <- within(RR, debtgdp <- ifelse(Country=="Netherlands",ifelse(Year<=1956,100*Debt/GDP1,100*Debt/GDP2),debtgdp))
-RR <- within(RR, debtgdp <- ifelse(Country=="New Zealand",ifelse(Year<=1947,100*Debt/GDP1,100*Debt/GDP2),debtgdp))
-RR <- within(RR, debtgdp <- ifelse(Country=="Norway",ifelse(Year<=1948,100*Debt/GDP1,100*Debt/GDP2),debtgdp))
-RR <- within(RR, debtgdp <- ifelse(Country=="Portugal",ifelse(Year<=1999,100*Debt1/GDP1,100*Debt2/GDP2),debtgdp))
-RR <- within(RR, debtgdp <- ifelse(Country=="Spain",ifelse(Year<=1957,100*Debt/GDP1,100*Debt/GDP2),debtgdp))
-RR <- within(RR, debtgdp <- ifelse(Country=="Sweden",ifelse(Year<=1949,100*Debt/GDP1,100*Debt/GDP2),debtgdp))
-RR <- within(RR, debtgdp <- ifelse(Country=="UK" , 100*Debt/GDP, debtgdp ))
-RR <- within(RR, debtgdp <- ifelse(Country=="US" , 100*Debt/GDP, debtgdp ))
+# Pick gdp value to use for debt ratio calculation -----------------------------
+# It's not obvious what rule governs which of GDP1, GDP2 and GNI to use
+# It looks like use GDP2 where available, otherwise falling back to GDP1.
+# I don't see how GNI fits in, given that it has very different units.
+gdp <- ifelse(!is.na(all$GDP2), all$GDP2, all$GDP1)
+all$debtgdp <- 100 * all$Debt / gdp
 
-RR$RGDP <- as.numeric(RR$RGDP)
-RR$RGDP1 <- as.numeric(RR$RGDP1)
-RR$RGDP2 <- as.numeric(RR$RGDP2)
+# Compute lagged gdps
+lg <- function(x) c(NA, x[-length(x)])
+all <- ddply(all, "Country", mutate,
+  lRGDP = lg(RGDP),
+  lRGDP1 = lg(RGDP1),
+  lRGDP2 = lg(RGDP2)
+)
 
-lg<-function(x)c(NA,x[1:(length(x)-1)])
-RR <- ddply( RR, ~Country, transform, lRGDP=lg(RGDP), lRGDP1=lg(RGDP1), lRGDP2=lg(RGDP2)  )
-RR <- within(RR, dRGDP <- ifelse( !is.na(dRGDP), dRGDP,
-                                   ifelse( !is.na( RGDP / lRGDP - 1 ), 100*(RGDP / lRGDP - 1) ,
-                                          ifelse( !is.na( RGDP2 / lRGDP2 - 1 ), 100*(RGDP2 / lRGDP2 - 1) ,
-                                                 ifelse( !is.na( RGDP1 / lRGDP1 - 1 ), 100*(RGDP1 / lRGDP1 - 1),dRGDP )))))
+# Fill in missing values of dRGDP ----------------------------------------------
+# Compute dRGDP, trying first RGDP, then RGDP2, then RGDP1
+dRGDP_poss <- with(all, cbind(
+  dRGDP,
+  RGDP / lRGDP - 1,
+  RGDP2 / lRGDP2 - 1,
+  RGDP1 / lRGDP1 - 1
+))
+colSums(is.na(dRGDP_poss))
 
+first_not_missing <- function(x) x[!is.na(x)][1]
+all$dRGDP <- apply(dRGDP_poss, 1, first_not_missing)
 
-RR <- subset(RR,Year>=1946 & Year<=2009 & !is.na(dRGDP) & !is.na(debtgdp))
-## Italy has another data series through 1946 and is excluded from GITD until 1951
-RR <- subset(RR, !(Year<1951 & Country=="Italy"))
+# Final tweaking ---------------------------------------------------------------
 
-write.dta(RR,"RR-processed.dta")
+all <- subset(all, Year >= 1946 & Year <= 2009 & !is.na(dRGDP) & !is.na(debtgdp))
+# Italy has another data series through 1946 and is excluded from GITD until 1951
+all <- subset(all, !(Year < 1951 & Country == "Italy"))
+
+# Remove columns that only consist of missing values
+col_na <- sapply(all, function(x) all(is.na(x)))
+all <- all[!col_na]
+
+write.csv(all, "RR-processed.csv", row.names = FALSE)
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Changes
 
 * Greece had commas in the RGDP column for 1991-2006. Previouly `as.numeric` was used to get rid of them which just turned them into NAs
+
+* It's unclear exactly how to pick between GDP, GDP1 and GDP2. I choose to use GDP2 where available, falling back to GDP1 where not available. This may be different from the original paper.
+
