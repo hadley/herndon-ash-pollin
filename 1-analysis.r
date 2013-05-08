6631daea91e376e1ca11b2f1e15d14c098a45bd4
hadley <h.wickham@gmail.com>
2013-05-08 07:42:16 -0500

    Split code into import and analysis

--- 1-analysis.r
+++ 1-analysis.r
@@ -7,92 +7,6 @@ options(digits=4)
 
 rm(list = ls())
 
-Australia     <- read.csv("RR - Australia.csv") 
-Austria       <- read.csv("RR - Austria.csv")   
-Belgium       <- read.csv("RR - Belgium.csv")   
-Canada        <- read.csv("RR - Canada.csv")    
-Denmark       <- read.csv("RR - Denmark.csv")   
-Finland       <- read.csv("RR - Finland.csv")   
-France        <- read.csv("RR - France.csv")    
-Germany       <- read.csv("RR - Germany.csv")   
-Greece        <- read.csv("RR - Greece.csv")    
-Ireland       <- read.csv("RR - Ireland.csv")   
-Italy         <- read.csv("RR - Italy.csv")     
-Japan         <- read.csv("RR - Japan.csv")     
-Netherlands   <- read.csv("RR - Netherlands.csv")
-NewZealand    <- read.csv("RR - New Zealand.csv")
-Norway        <- read.csv("RR - Norway.csv")
-Portugal      <- read.csv("RR - Portugal.csv")
-Spain         <- read.csv("RR - Spain.csv")
-Sweden        <- read.csv("RR - Sweden.csv")
-UK            <- read.csv("RR - UK.csv")
-US            <- read.csv("RR - US.csv")
-
-RR <- merge(Australia,Austria,all=TRUE)
-RR <- merge(RR,Belgium    ,all=TRUE)
-RR <- merge(RR,Canada     ,all=TRUE)
-RR <- merge(RR,Denmark    ,all=TRUE)
-RR <- merge(RR,Finland    ,all=TRUE)
-RR <- merge(RR,France     ,all=TRUE)
-RR <- merge(RR,Germany    ,all=TRUE)
-RR <- merge(RR,Greece     ,all=TRUE)
-RR <- merge(RR,Ireland    ,all=TRUE)
-RR <- merge(RR,Italy      ,all=TRUE)
-RR <- merge(RR,Japan      ,all=TRUE)
-RR <- merge(RR,Netherlands,all=TRUE)
-RR <- merge(RR,NewZealand ,all=TRUE)
-RR <- merge(RR,Norway     ,all=TRUE)
-RR <- merge(RR,Portugal   ,all=TRUE)
-RR <- merge(RR,Spain      ,all=TRUE)
-RR <- merge(RR,Sweden     ,all=TRUE)
-RR <- merge(RR,UK         ,all=TRUE)
-RR <- merge(RR,US         ,all=TRUE)
-
-with(RR,table(Country))
-
-RR$debtgdp <- RR$debtgdp
-
-write.dta(RR,"RR-basic.dta")
-
-RR <- within(RR, debtgdp <- ifelse(Country=="Australia",ifelse(Year<=1948,100*Debt/GDP1,100*Debt/GDP2),debtgdp))
-RR <- within(RR, debtgdp <- ifelse(Country=="Austria",ifelse(Year<=1979,100*Debt1/GDP1,100*Debt2/GDP2),debtgdp))
-RR <- within(RR, debtgdp <- ifelse(Country=="Belgium",ifelse(Year<=1979,100*Debt/GDP1,100*Debt/GDP2),debtgdp))
-RR <- within(RR, debtgdp <- ifelse(Country=="Canada",ifelse(Year<=1948,100*Debt/GDP1,100*Debt/GDP2),debtgdp))
-RR <- within(RR, debtgdp <- ifelse(Country=="Denmark",ifelse(Year<=1949,100*Debt1/GDP1,100*Debt1/GDP2),debtgdp))
-RR <- within(RR, debtgdp <- ifelse(Country=="Finland",ifelse(Year<=1977,100*Debt1/GDP1,100*Debt2/GDP2),debtgdp))
-RR <- within(RR, debtgdp <- ifelse(Country=="France",ifelse(Year<=1948, 100*Debt1 / GDP1, ifelse(Year<=1978, 100*Debt1 / GDP2,100*Debt2/GDP2)),debtgdp))
-RR <- within(RR, debtgdp <- ifelse(Country=="Germany",ifelse(Year<=1950,100*Debt1/GDP1,100*Debt2/GDP2),debtgdp))
-RR <- within(RR, debtgdp <- ifelse(Country=="Greece",ifelse((Year>=1884 & Year<=1913) | (Year>=1919 & Year<=1939) | (Year>=1970 & Year<=1992),100*Debt/GDP1, ifelse(Year==2009,100,debtgdp)),debtgdp))
-RR <- within(RR, debtgdp <- ifelse(Country=="Ireland",ifelse(Year<=2010,100*Debt/GDP2,debtgdp),debtgdp))
-RR <- within(RR, debtgdp <- ifelse(Country=="Italy",ifelse(Year<=1913,100*Debt/GDP1,ifelse(Year<=1946,100*Debt/GNI,ifelse(Year<=1998,100*Debt/GDP1,100*Debt/GDP2))),debtgdp))
-RR <- within(RR, debtgdp <- ifelse(Country=="Japan",ifelse(Year<=1953,100*Debt/GDP1,100*Debt/GDP2),debtgdp))
-RR <- within(RR, debtgdp <- ifelse(Country=="Netherlands",ifelse(Year<=1956,100*Debt/GDP1,100*Debt/GDP2),debtgdp))
-RR <- within(RR, debtgdp <- ifelse(Country=="New Zealand",ifelse(Year<=1947,100*Debt/GDP1,100*Debt/GDP2),debtgdp))
-RR <- within(RR, debtgdp <- ifelse(Country=="Norway",ifelse(Year<=1948,100*Debt/GDP1,100*Debt/GDP2),debtgdp))
-RR <- within(RR, debtgdp <- ifelse(Country=="Portugal",ifelse(Year<=1999,100*Debt1/GDP1,100*Debt2/GDP2),debtgdp))
-RR <- within(RR, debtgdp <- ifelse(Country=="Spain",ifelse(Year<=1957,100*Debt/GDP1,100*Debt/GDP2),debtgdp))
-RR <- within(RR, debtgdp <- ifelse(Country=="Sweden",ifelse(Year<=1949,100*Debt/GDP1,100*Debt/GDP2),debtgdp))
-RR <- within(RR, debtgdp <- ifelse(Country=="UK" , 100*Debt/GDP, debtgdp ))
-RR <- within(RR, debtgdp <- ifelse(Country=="US" , 100*Debt/GDP, debtgdp ))
-
-RR$RGDP <- as.numeric(RR$RGDP)
-RR$RGDP1 <- as.numeric(RR$RGDP1)
-RR$RGDP2 <- as.numeric(RR$RGDP2)
-
-lg<-function(x)c(NA,x[1:(length(x)-1)])
-RR <- ddply( RR, ~Country, transform, lRGDP=lg(RGDP), lRGDP1=lg(RGDP1), lRGDP2=lg(RGDP2)  )
-RR <- within(RR, dRGDP <- ifelse( !is.na(dRGDP), dRGDP,
-                                   ifelse( !is.na( RGDP / lRGDP - 1 ), 100*(RGDP / lRGDP - 1) ,
-                                          ifelse( !is.na( RGDP2 / lRGDP2 - 1 ), 100*(RGDP2 / lRGDP2 - 1) ,
-                                                 ifelse( !is.na( RGDP1 / lRGDP1 - 1 ), 100*(RGDP1 / lRGDP1 - 1),dRGDP )))))
-
-
-RR <- subset(RR,Year>=1946 & Year<=2009 & !is.na(dRGDP) & !is.na(debtgdp))
-## Italy has another data series through 1946 and is excluded from GITD until 1951
-RR <- subset(RR, !(Year<1951 & Country=="Italy"))
-
-write.dta(RR,"RR-processed.dta")
-
 with(RR,table(Year))
 with(RR,table(Country))
 
@@ -181,7 +95,7 @@ RR.newzealand.1951 <- subset(RR.selective.spreadsheet,Country=="New Zealand" & Y
 
 ## Categorical scatterplot
 n <- ggplot(RR, aes(x=dgcat,y=dRGDP)) + geom_point(shape=3,color='darkgray') + ylab("Real GDP Growth") + xlab("Public Debt/GDP Category")
-n <- n + geom_point(RR.published.mean.df, mapping=aes(x=dgcat,y=RR.published.mean), shape=5,  size=5 ) 
+n <- n + geom_point(RR.published.mean.df, mapping=aes(x=dgcat,y=RR.published.mean), shape=5,  size=5 )
 n <- n + geom_text(RR.published.mean.df, mapping=aes(x=dgcat,y=RR.published.mean,label=round(RR.published.mean,1)),hjust=-0.7,size=3,color='darkgray')
 n <- n + geom_point(RR.correct.mean.df,  mapping=aes(x=dgcat,y=RR.correct.mean,label=RR.correct.mean), shape=16, size=4 )  + theme_bw()
 n <- n + geom_text(RR.correct.mean.df,  mapping=aes(x=dgcat,y=RR.correct.mean,label=round(RR.correct.mean,1)), hjust=1.7,size=3,color='darkgray')
@@ -222,7 +136,7 @@ print(m1)
 ## Scatterplot closeup
 pdf("closeup.pdf",height=4,width=7)
 m2 <- m + geom_point(color='darkgray') + ylab("Real GDP Growth") + xlab("Public Debt/GDP Ratio") + scale_x_continuous(breaks=seq(0,240,30)) + theme_bw() +  geom_vline(xintercept=90,color='lightgray',size=1.5)
-## m2 <- m2 + geom_smooth(method='loess',span=0.75,color='black') + geom_smooth(method='loess',span=0.4,color='black') 
+## m2 <- m2 + geom_smooth(method='loess',span=0.75,color='black') + geom_smooth(method='loess',span=0.4,color='black')
 ## m2 <- m2 + geom_smooth(method='auto',color='black')
 m2 <- m2 + geom_smooth(method=gam, color='black',formula= y ~ s(x, bs = "cs"))
 m2 <- m2 + coord_cartesian(ylim=c(0, 7),xlim=c(0,150)) + scale_y_continuous(breaks=c(0,1,2,3,4,5,6,7)) + theme_bw()
